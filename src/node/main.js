9f4f25f79c68af07bea1cd1fd27580b1f2c4c539
krircc <krircc@aliyun.com>
2019-10-31 09:57:24 +0800

    update doc cn

--- src/node/main.js
+++
@@ -1,8 +0,0 @@
-let http = require('http');
-
-http.createServer(function(req, res){
-    res.writeHead(200, {'Content-Type' : 'text/plain'});
-    res.end('hello world');
-}).listen(8080,'127.0.0.1');
-
-console.log('server is running..');
